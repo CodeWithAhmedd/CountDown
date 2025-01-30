b92d3442dd7de34f2143e588d64372cb41473806
Muhammad Ahmed <155528907+CodeWithAhmedd@users.noreply.github.com>
2025-01-30 12:13:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("June 15, 2024 00:00:00").getTime();
+var countDownDate = new Date("June 15, 2025 00:00:00").getTime();
     var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
